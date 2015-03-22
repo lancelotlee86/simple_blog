05c4d0ad5d4eb85c57f37faddc01d0bb5b77ba2c
lancelotlee86 <lancelotlee86@gmail.com>
2015-03-22 10:01:27 +0800

    c
    run at local

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import app
 imports the app variable from our app package and invokes its run method
 the app variable holds the Flask instance
 '''
-#app.run(host = "127.0.0.1", port = 5000, debug = True)
+app.run(host = "127.0.0.1", port = 5000, debug = True)
 # only use when test on your own computer
