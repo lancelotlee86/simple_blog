d65f3509bf1432bf3c01db868e211115fa359aec
lancelotlee86 <lancelotlee86@gmail.com>
2015-05-05 21:53:56 +0800

    11
    11

--- app/static/js/js.js
+++ app/static/js/js.js
@@ -11,6 +11,18 @@ function addLoadEvent(func) {
   }
 }
 
+// tool function
+function addClass(element,value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName+= " ";
+    newClassName+= value;
+    element.className = newClassName;
+  }
+}
+
 // 改变所有class名为time_from_now的时间戳标签的内容，转换为距离今年的时间
 function timeFromNow() {
   var timestamps = document.getElementsByClassName("time_from_now");
@@ -21,4 +33,21 @@ function timeFromNow() {
   }
 }
 
+// 为当前所在页面的导航标签添加 class="here",便于更改样式
+function highlightPage() {
+  var navbar = document.getElementsByClassName("navbar");
+  // 取得导航链接
+  var links = navbar[0].getElementsByTagName("a");
+  var linkurl;
+  for (var i = 0; i < links.length; i++) {
+      // 遍历导航链接，比较当前链接的 URL 与当前页面的 URL。
+      linkurl = links[i].getAttribute("href");
+      if (window.location.href.indexOf(linkurl) != -1) {
+        // window.location.href 用来表示当前页面的网址，
+        // indexOf() 函数返回 -1 时，表示字符串不再里面。
+        addClass(links[i],"here");
+      }
+  }
+}
 addLoadEvent(timeFromNow);
+addLoadEvent(highlightPage);
